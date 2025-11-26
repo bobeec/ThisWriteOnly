edf175249a2545f834e056c279ed3b764c67e740
Rec <genspark_dev@genspark.ai>
2025-11-26 07:30:20 +0000

    feat: Add English (en_US) translation (v0.4.1)
    - Update POT template file with all current translatable strings
    - Add English translation (en_US.po and en_US.mo)
    - Translatable strings include:
      - Navigation and UI elements
      - Breadcrumb labels
      - Comment form and display
      - Archive titles
      - Search and 404 pages
      - Reading time display

--- functions.php
+++ functions.php
@@ -5,14 +5,14 @@
  * 「書くこと。読んでもらうこと。」
  *
  * @package BLOGthemeWP
- * @version 0.4.0
+ * @version 0.4.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'BLOGTHEMEWP_VERSION', '0.4.0' );
+define( 'BLOGTHEMEWP_VERSION', '0.4.1' );
 define( 'BLOGTHEMEWP_DIR', get_template_directory() );
 define( 'BLOGTHEMEWP_URI', get_template_directory_uri() );
 
